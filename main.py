713d2dac1d68e5839891c741faee2ce240d248e8
ducnm51 <v.ducnm51@vinai.io>
2022-10-11 00:04:46 +0700

    fix bug when a and b is the same

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ from game_data import data
 is_playing = True
 score = 0
 
+def format_choices(item):
+    return f"{item['name']}, a {item['description']}, from {item['country']}."
+
 def check_answer(a, b):
     if a > b:
         return "A"
     elif b > a:
         return "B"
 
-def play(c, answer):
+def play(user_c, answer):
     global score
     global is_playing
 
-    if c == answer:
+    if user_c == answer:
         score += 1
         print(f"You're right! Current score: {score}")
-    elif c != answer:
+    elif user_c != answer:
         is_playing = False
         print(f"Sorry, that's wrong. Final score: {score}")
 
@@ -28,9 +31,14 @@ print(logo)
 while is_playing:
     A = choice(data)
     B = choice(data)
-    print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
+
+    while A["name"] == B["name"]:
+        B = choice(data)
+
+    print(f"Compare A: {format_choices(A)}")
     print(vs)
-    print(f"Compare B: {B['name']}, a {B['description']}, from {B['country']}.")
+    print(f"Compare B: {format_choices(B)}")
+
     user_choice = input("Who has more folowers? Type 'A' or 'B': ").upper()
 
     answer = check_answer(A['follower_count'] , B['follower_count'])
